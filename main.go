83e3f85c78fb2b8d9b2855c6cb8c430aed980073
huyukang <huyukang@moresec.cn>
2023-02-03 15:11:11 +0800

    update

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path"
+	"path/filepath"
 	"text/template"
 
 	"github.com/gocolly/colly"
@@ -13,38 +17,107 @@ const (
 	BedrockDownloadPage        = "https://www.minecraft.net/en-us/download/server/bedrock"
 	BedrockLinkSelector        = "a[data-platform='serverBedrockLinux']"
 	BedrockLinkPreviewSelector = "a[data-platform='serverBedrockPreviewLinux']"
+	DefaultBedrockLink         = "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.52.01.zip"
 )
 
 var (
 	InstallRootDir = "/opt"
 	WorkDir        = path.Join(os.TempDir(), "minecraft_kainhuck")
+	Version        = ""
+	DownloadLink   = "https://minecraft.azureedge.net/bin-linux/bedrock-server-%s.zip"
 )
 
-func main() {
-	link, err := GetBedrockDownloadLink(BedrockLinkSelector)
-	if err != nil {
-		log.Fatalf("get link failed: %v", err)
-	}
+func init() {
+	flag.StringVar(&InstallRootDir, "i", InstallRootDir, "-i <安装目录>")
+	flag.StringVar(&Version, "v", "", "-v <安装版本>")
+	flag.Parse()
+}
 
-	if err := TemplateDockerfile(link); err != nil {
-		log.Fatalf("get dockerfile failed: %v", err)
+func main() {
+	// 0. 检查环境docker，docker-compose是否安装
+	CheckEnv()
+	// 1. 进入工作目录，打包镜像
+	defer NewDir(WorkDir)()
+	// 	1.1 如果用户指定了版本则使用用户指定的版本，否则去网站上拉去，如果拉取失败则使用默认的url
+	if Version != "" {
+		DownloadLink = fmt.Sprintf(DownloadLink, Version)
+	} else {
+		link, _ := GetBedrockDownloadLink(BedrockLinkSelector)
+		if link == "" {
+			DownloadLink = DefaultBedrockLink
+		} else {
+			DownloadLink = link
+		}
 	}
-	log.Println("SUCCESS")
+	// 	1.2 新建Dockerfile，构建docker镜像
+	if err := TemplateDockerfile(DownloadLink); err != nil {
+		log.Fatal(err)
+	}
+	version, err := getVersion(DownloadLink)
+	if err != nil {
+		log.Fatal(err)
+	}
+	image := fmt.Sprintf("kainhuck/bedrock:%s", version)
+	runCmd(fmt.Sprintf("docker build -t %s -f %s %s", image, filepath.Join(WorkDir, "Dockerfile"), WorkDir))
+	// 	1.3 删除工作目录
+
+	// 2. 创建安装目录
+	installDir := filepath.Join(InstallRootDir, "minecraft_kainchuk")
+	NewDir(installDir)
+	// 	2.1 生成各种配置文件
+	NewDir(filepath.Join(installDir, "worlds"))
+	if err := TemplateBedrock(installDir, image); err != nil {
+		log.Fatal(err)
+	}
+	//	2.2 启动服务
+
+	// link, err := GetBedrockDownloadLink(BedrockLinkSelector)
+	// if err != nil {
+	// 	log.Fatalf("get link failed: %v", err)
+	// }
+
+	// if err := TemplateDockerfile(link); err != nil {
+	// 	log.Fatalf("get dockerfile failed: %v", err)
+	// }
+	// log.Println("SUCCESS")
 }
 
 func TemplateDockerfile(link string) error {
 
-	dockerfileTemp, err := template.New("xxxx").Parse(DockerfileTemp)
+	dockerfileTemp, err := template.New("docker").Parse(DockerfileTemp)
 	if err != nil {
 		return err
 	}
 
-	f, err := os.OpenFile("Dockerfile", os.O_CREATE|os.O_WRONLY, 0755)
+	f, err := os.OpenFile(filepath.Join(WorkDir, "Dockerfile"), os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		return err
 	}
 
 	return dockerfileTemp.Execute(f, link)
+}
+
+func TemplateBedrock(installDir string, image string) error {
+
+	dockerCompose := DockerCompose{
+		Image:      image,
+		InstallDir: installDir,
+	}
+
+	dockercomposeTemp, err := template.New("docker-compose").Parse(DockercomposeTemp)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(filepath.Join(installDir, "docker-compose.yml"), os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		return err
+	}
+	if err := dockercomposeTemp.Execute(f, dockerCompose); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func GetBedrockDownloadLink(selector string) (link string, err error) {
@@ -56,4 +129,13 @@ func GetBedrockDownloadLink(selector string) (link string, err error) {
 
 	err = c.Visit(BedrockDownloadPage)
 	return
+}
+
+func CheckEnv() {
+	if _, err := exec.LookPath("docker"); err != nil {
+		log.Fatal("docker not install")
+	}
+	if _, err := exec.LookPath("docker-compose"); err != nil {
+		log.Fatal("docker-compose not install")
+	}
 }
---
+++ utils.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+)
+
+// ExistPath 判断路径是否存在
+func ExistPath(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) { //如果返回的错误类型使用os.isNotExist()判断为true，说明文件或者文件夹不存在
+		return false, nil
+	}
+	return false, err //如果有错误了，但是不是不存在的错误，所以把这个错误原封不动的返回
+}
+
+// DeletePath 删除路径
+func DeletePath(path string) error {
+	return os.RemoveAll(path)
+}
+
+// NewPath 新建路径
+func NewPath(path string) error {
+	return os.MkdirAll(path, 0755)
+}
+
+func runCmd(cmd string) error {
+	log.Println(cmd)
+	c := exec.Command("/bin/sh", "-c", cmd)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	return c.Run()
+}
+
+func getVersion(link string) (string, error) {
+	// https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.52.01.zip
+	re, err := regexp.Compile(`server-(.+?).zip`)
+	if err != nil {
+		return "", err
+	}
+	result := re.FindAllStringSubmatch(link, 1)
+
+	if len(result) == 0 {
+		return "", fmt.Errorf("not found version")
+	}
+
+	return result[0][1], nil
+}
+
+func NewDir(dir string) func() {
+	exist, err := ExistPath(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if exist { // 这个目录可以安全删除
+		if err := DeletePath(dir); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := NewPath(dir); err != nil {
+		log.Fatal(err)
+	}
+
+	return func() {
+		_ = DeletePath(dir)
+	}
+}
