834d305ab356d1ed1e9a5828bb82489bd41dbbf8
huyukang <huyukang@moresec.cn>
2023-02-03 16:06:14 +0800

    add build

---
+++ go_build_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+import sys
+
+os_arch = {
+    "darwin": [
+        "amd64",
+        "arm64"
+    ],
+    "linux": [
+        "386",
+        "amd64",
+        "arm",
+        "arm64",
+    ],
+    "windows": [
+        "386",
+        "amd64"
+    ]
+}
+pkg_name = "bsd"
+
+
+def build_all(name: str):
+    for os_, arch_s in os_arch.items():
+        for arch in arch_s:
+            if os_ == "windows":
+                cmd = f"CGO_ENABLED=0 GOOS={os_} GOARCH={arch} go build -o build/{name}_{os_}_{arch}.exe"
+                print(cmd)
+                os.system(cmd)
+            else:
+                cmd = f"CGO_ENABLED=0 GOOS={os_} GOARCH={arch} go build -o build/{name}_{os_}_{arch}"
+                print(cmd)
+                os.system(cmd)
+
+
+if __name__ == '__main__':
+    build_all(pkg_name)
